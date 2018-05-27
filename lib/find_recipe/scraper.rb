6141cd4a85b2fe1a1aba1e174fc3f8b6594b34fa
Felice Forby <forby.1@gmail.com>
2018-05-27 11:41:09 +0900

    Add individual recipe page scraper to Scraper class

--- lib/find_recipe/scraper.rb
+++ lib/find_recipe/scraper.rb
@@ -5,7 +5,6 @@ class FindRecipe::Scraper
 		recipes = []
 		
 		recipe_page = self.get_trending_recipes_page
-		# binding.pry
 		
 		recipe_cards = recipe_page.css( ".card.feed__card" )
 		recipe_cards.collect do |card|
@@ -14,11 +13,29 @@ class FindRecipe::Scraper
 					recipe_url: ROOT_URL + card.css( "a.link-unstyled" ).attribute( "href" ).value
 				}
 		end
+	end
+	
+	def self.scrape_individual_recipe_data( recipe_url )
+		recipe_data = {}
+
+		recipe_page = self.get_individual_recipe_page( recipe_url )
+		recipe_data[:description] = recipe_page.at("meta[itemprop='description']")['content']
 		
+		# Gets ingredients as an array of each ingredient
+		recipe_data[:ingredients] = recipe_page.css( ".ingredient__details" ).collect{ |ingredient| ingredient.text.strip }
+		
+		# Gets steps as an array of each step
+		recipe_data[:steps] = recipe_page.css(".step .prose").collect{ |step| step.text.strip }
+
+		recipe_data
 	end
 	
 	def self.get_trending_recipes_page
 		url = ROOT_URL + "/uk/trending"
 		Nokogiri::HTML( open( url ) )
+	end
+	
+	def self.get_individual_recipe_page( recipe_url )
+		Nokogiri::HTML( open( recipe_url ))
 	end
 end
