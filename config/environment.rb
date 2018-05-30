d881812a6939d04c680f80db3d56a77140632399
Felice Forby <forby.1@gmail.com>
2018-05-31 08:00:11 +0900

    move requirements into environment file

---
+++ config/environment.rb
@@ -0,0 +1,12 @@
+require 'open-uri'
+require 'nokogiri'
+require 'colorize'
+# Uncomment below if you need to use pry for debugging, etc.
+# require 'pry'
+
+require_relative "../lib/find_recipe/version"
+require_relative "../lib/find_recipe/cli"
+require_relative "../lib/find_recipe/recipe"
+require_relative "../lib/find_recipe/searched_recipe"
+require_relative "../lib/find_recipe/trending_recipe"
+require_relative "../lib/find_recipe/scraper"
--- lib/find_recipe.rb
+++ lib/find_recipe.rb
@@ -1,15 +1,4 @@
-# Loads the needed files.
-require 'open-uri'
-require 'nokogiri'
-require 'colorize'
-# Uncomment below if you need to use pry for debugging, etc.
-# require 'pry'
-
-require_relative "find_recipe/version"
-
 module FindRecipe
 end
 
-require_relative "find_recipe/cli"
-require_relative "find_recipe/recipe"
-require_relative "find_recipe/scraper"
+require_relative '../config/environment'
